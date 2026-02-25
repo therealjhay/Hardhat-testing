6178a12c63d1c3b45d71fbf4ff302b49b85c8dd0
Johnson Oyemade <johnsonoca@gmail.com>
2026-02-25 09:50:22 +0100

    feat: successfully tested erc20 token value and save ether function

--- test/SaveAsset.ts
+++ test/SaveAsset.ts
@@ -1,127 +1,45 @@
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-import {
-  time,
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { ethers } from "hardhat";
 import { expect } from "chai";
-import hre from "hardhat";
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe("SaveAsset", function () {
+	let erc20Token: any, saveAsset: any, addr1: any, addr2: any, addr3: any;
+	
+	beforeEach(async function () {
+		const ERC20Token = await ethers.getContractFactory("ERC20");
+		const SaveAsset = await ethers.getContractFactory("SaveAsset");
+		[addr1, addr2, addr3] = await ethers.getSigners();
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+		erc20Token = await ERC20Token.deploy();
+		await erc20Token.waitForDeployment();
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await hre.ethers.getSigners();
+		const erc20TokenAddress = await erc20Token.getAddress();
 
-    const Lock = await hre.ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+		saveAsset = await SaveAsset.deploy(erc20TokenAddress);
+		await saveAsset.waitForDeployment();
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
+		const saveAssetAddress = await saveAsset.getAddress();
+	});
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+	it("should correctly return the name, symbol and decimal for the erc20 token", async function () {
+		expect(await erc20Token.name()).to.eq("UnivToken");
+		expect(await erc20Token.symbol()).to.eq("Univt");
+		expect(await erc20Token.decimals()).to.eq(18);
+	});
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
+	it("should be able to deposit ether in save asset contract", async function (){
+		const depositAmount = ethers.parseEther("2");
 
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+    await erc20Token.mint(addr1.getAddress(), ethers.parseEther("10"));
+    await erc20Token.connect(addr1).approve(await saveAsset.getAddress(), depositAmount);
 
-      expect(await lock.owner()).to.equal(owner.address);
-    });
+    const allowance = await erc20Token.allowance(addr1.getAddress(), await saveAsset.getAddress());
+    expect(allowance).to.eq(depositAmount);
+    
+    // await saveAsset.connect(addr1).deposit({ value : depositAmount});
+		// expect(await saveAsset.connect(addr1).getUserSavings()).to.eq(depositAmount);
 
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture,
-      );
 
-      expect(await hre.ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount,
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await hre.ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future",
-      );
-    });
-  });
-
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet",
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture,
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner",
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture,
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture,
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture,
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount],
-        );
-      });
-    });
-  });
 });
+
+   
+	});
